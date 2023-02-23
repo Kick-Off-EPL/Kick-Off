0939890516f31da3cf0f81d9e623a577124d4273
Joon Hwang <joonhwangs@gmail.com>
2023-02-22 23:48:52 -0500

    tables and index

--- kick-off/src/components/Tables.tsx
+++ kick-off/src/components/Tables.tsx
@@ -1,6 +1,97 @@
 // import { type any } from "next";
+// import React, { useState } from 'react';
 
-const Tables: any = () => {
-  return();
+// const Tables: any = () => {
+//   return(
+//     <div>
+//       <div>
+//         Hi
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Tables;
+
+import React from 'react';
+
+type Listing = {
+  id: number;
+  description: string;
+  price: number;
 };
-  export default Tables;
+
+type ListingsTableProps = {
+  listings: Listing[] | undefined;
+};
+
+const ListingsTable = ({ listings }: ListingsTableProps) => {
+  console.log(listings);
+  const table1 = listings?.slice(0, 20);
+  const table2 = listings?.slice(20, 40);
+  const table3 = listings?.slice(40, 60);
+
+  return (
+    <div id="listings">
+      <table>
+        <thead>
+          <tr>
+            <th>Listing #</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table1 &&
+            table1.map((listing) => (
+              <tr key={listing.id}>
+                <td>{listing.id}</td>
+                <td>{listing.description}</td>
+                <td>{listing.price}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Listing #</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table2 &&
+            table2.map((listing) => (
+              <tr key={listing.id}>
+                <td>{listing.id}</td>
+                <td>{listing.description}</td>
+                <td>{listing.price}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Listing #</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table3 &&
+            table3.map((listing) => (
+              <tr key={listing.id}>
+                <td>{listing.id}</td>
+                <td>{listing.description}</td>
+                <td>{listing.price}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListingsTable;
--- kick-off/src/pages/index.tsx
+++ kick-off/src/pages/index.tsx
@@ -2,8 +2,11 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Tables from '../components/Tables';
+import React, { useState } from 'react';
 
 const Home: NextPage = () => {
+
+  const [players, setPlayers] = useState(['Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', 'Marco Reus', 'Lewandowski', 'Marcus Rashford', 'Kylian Mbappe', ]);
   return (
     <>
       <Head>
@@ -14,9 +17,21 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            Create <span className="text-[hsl(280,100%,70%)]">EPL</span> Kick-Off
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <div
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://create.t3.gg/en/usage/first-steps"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">First Steps →</h3>
+                <div className="text-lg">
+                  <Tables players={players}/>
+                </div>
+            </div>
+          </div>
+          {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
@@ -39,7 +54,7 @@ const Home: NextPage = () => {
                 to deploy it.
               </div>
             </Link>
-          </div>
+          </div> */}
         </div>
       </main>
     </>
