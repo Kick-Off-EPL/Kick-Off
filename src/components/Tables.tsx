25e32f7bbbb29fb5d707c810808db85b0cb0e01b
Joon Hwang <joonhwangs@gmail.com>
2023-02-23 19:41:59 -0500

    added goals, assists, and yellow cards table for Kick-Off app

--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,91 +1,75 @@
-// import { type any } from "next";
-// import React, { useState } from 'react';
-
-// const Tables: any = () => {
-//   return(
-//     <div>
-//       <div>
-//         Hi
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Tables;
-
 import React from 'react';
 
 type Listing = {
   id: number;
-  description: string;
-  price: number;
+  name: string;
+  goals: number;
+  assists: number;
+  yellow_cards: number;
 };
 
 type ListingsTableProps = {
-  listings: Listing[] | undefined;
+  players: Listing[] | undefined;
 };
 
-const ListingsTable = ({ listings }: ListingsTableProps) => {
-  console.log(listings);
-  const table1 = listings?.slice(0, 20);
-  const table2 = listings?.slice(20, 40);
-  const table3 = listings?.slice(40, 60);
+const ListingsTable = ({ players }: ListingsTableProps) => {
+  const data = players?.slice();
 
   return (
-    <div id="listings">
-      <table>
+    <div id="listings" style={{ display: 'flex' }}>
+      <table style={{ flex: 1, width: '100%', borderCollapse: 'collapse', margin: '0 10px' }}>
         <thead>
-          <tr>
-            <th>Listing #</th>
-            <th>Description</th>
-            <th>Price</th>
+          <tr style={{ borderBottom: '2px solid black' }}>
+            <th style={{ padding: '10px 0', textAlign: 'left' }}></th>
+            <th style={{ padding: '10px 0', textAlign: 'left'}}>Players</th>
+            <th style={{ padding: '10px 0', textAlign: 'center' }}>Goals</th>
           </tr>
         </thead>
         <tbody>
-          {table1 &&
-            table1.map((listing) => (
-              <tr key={listing.id}>
-                <td>{listing.id}</td>
-                <td>{listing.description}</td>
-                <td>{listing.price}</td>
+          {data &&
+            data.map((player, index) => (
+              <tr key={player.id} style={{ borderBottom: '1px solid black' }}>
+                <td style={{ padding: '10px 0', paddingRight: '20px' }}>{`${index + 1}`}</td>
+                <td style={{ padding: '10px 0', minWidth: '250px' }}>{`${player.name}`}</td>
+                <td style={{ padding: '10px 0', textAlign: 'center' }}>{player.goals}</td>
               </tr>
             ))}
         </tbody>
       </table>
-      <table>
+      <table style={{ flex: 1, width: '100%', borderCollapse: 'collapse', margin: '0 10px' }}>
         <thead>
-          <tr>
-            <th>Listing #</th>
-            <th>Description</th>
-            <th>Price</th>
+          <tr style={{ borderBottom: '2px solid black' }}>
+            <th style={{ padding: '10px 0', colSpan: 1, textAlign: 'left' }}></th>
+            <th style={{ padding: '10px 0', colSpan: 2, textAlign: 'left' }}>Players</th>
+            <th style={{ padding: '10px 0', textAlign: 'center' }}>Assists</th>
           </tr>
         </thead>
         <tbody>
-          {table2 &&
-            table2.map((listing) => (
-              <tr key={listing.id}>
-                <td>{listing.id}</td>
-                <td>{listing.description}</td>
-                <td>{listing.price}</td>
+          {data &&
+            data.map((player, index) => (
+              <tr key={player.id} style={{ borderBottom: '1px solid black' }}>
+                <td style={{ padding: '10px 0', paddingRight: '20px'}}>{`${index + 1}`}</td>
+                <td style={{ padding: '10px 0', minWidth: '250px' }}>{`${player.name}`}</td>
+                <td style={{ padding: '10px 0', textAlign: 'center' }}>{player.assists}</td>
               </tr>
             ))}
         </tbody>
       </table>
-      <table>
+      <table style={{ flex: 1, width: '30%', borderCollapse: 'collapse', margin: '0 10px' }}>
         <thead>
-          <tr>
-            <th>Listing #</th>
-            <th>Description</th>
-            <th>Price</th>
+          <tr style={{ borderBottom: '2px solid black' }}>
+            <th style={{ padding: '10px 0', colSpan: 1, textAlign: 'left' }}></th>
+            <th style={{ padding: '10px 0', minWidth: '160px', textAlign: 'left' }}>Players</th>
+            <th style={{ padding: '10px 0', textAlign: 'center' }}>🟨</th>
           </tr>
         </thead>
         <tbody>
-          {table3 &&
-            table3.map((listing) => (
-              <tr key={listing.id}>
-                <td>{listing.id}</td>
-                <td>{listing.description}</td>
-                <td>{listing.price}</td>
+          {data &&
+            data.map((player, index) => (
+              <tr key={player.id} style={{ borderBottom: '1px solid black' }}>
+                <td style={{ padding: '10px 0', paddingRight: '20px' }}>{`${index + 1}`}</td>
+                <td style={{ padding: '10px 0', minWidth: '250px' }}>{`${player.name}`}</td>
+                <td style={{ padding: '10px 0', textAlign: 'center' }}>{player.yellow_cards}</td>
               </tr>
             ))}
         </tbody>
